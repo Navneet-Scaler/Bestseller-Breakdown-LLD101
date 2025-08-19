b6e18fc8b85ce1551855d517d804a6e61327f56c
Navneet-Scaler <navneet.23bcs10136@ms.sst.scaler.com>
2025-08-19 21:45:33 +0530

    Load book dataset using DatasetReader in main

--- Driver.java
+++ Driver.java
@@ -1,3 +1,14 @@
+import java.util.List;
+import java.util.Scanner;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Driver {
-    
+
+    private static List<Book> dataset;
+
+    public static void main(String[] args) {
+        dataset = DatasetReader.readDataset("bestsellers.csv");
+        System.out.println("Successfully loaded " + dataset.size() + " books.");
+    }
 }
